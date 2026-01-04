606caffe316f357db310483ce825c28578968560
Emmanuel Adoum <adoumouangnamouemmanuel@gmail.com>
2026-01-04 18:43:26 +0000

    Add: ajouter view manager

---
+++ src/main/java/com/pharmasys/views/ViewManager.java
@@ -0,0 +1,60 @@
+package com.pharmasys.views;
+
+import com.pharmasys.controllers.DashboardController;
+import com.pharmasys.controllers.LoginController;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Gestionnaire de navigation entre les vues
+ */
+public class ViewManager {
+    private static Stage primaryStage;
+    
+    /**
+     * Affiche la vue de connexion
+     */
+    public static void showLoginView(Stage stage) {
+        primaryStage = stage;
+        
+        try {
+            LoginController controller = new LoginController();
+            Scene scene = new Scene(controller.getView(), 400, 300);
+            
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("PharmaSys - Connexion");
+            primaryStage.setResizable(false);
+            
+        } catch (Exception e) {
+            // e.printStackTrace();
+            System.err.println("Erreur lors du chargement de la vue de connexion");
+        }
+    }
+    
+    /**
+     * Affiche le tableau de bord principal
+     */
+    public static void showDashboard() {
+        try {
+            DashboardController controller = new DashboardController();
+            Scene scene = new Scene(controller.getView(), 1024, 768);
+            
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("PharmaSys - Tableau de Bord");
+            primaryStage.setResizable(true);
+            primaryStage.setMaximized(true);
+            
+        } catch (Exception e) {
+            // e.printStackTrace();
+            System.err.println("Erreur lors du chargement du tableau de bord");
+        }
+    }
+    
+    /**
+     * Retourne le stage principal
+     */
+    public static Stage getPrimaryStage() {
+        return primaryStage;
+    }
+}
